811f2357b605eb3907903243b158fde0794f3c90
bokang03 <bogang12345@gmail.com>
2025-11-27 14:03:30 +0900

    문제 : 삽입 정렬 기본 예제

---
+++ 이코테/정렬 알고리즘/삽입정렬.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__' :
+    arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+    for i in range(len(arr)) :
+        for j in range(i, 0, -1) :
+            if arr[j] < arr[j-1] :
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                print(i, j, " =", arr)
+            else :
+                break
+    print(arr)
