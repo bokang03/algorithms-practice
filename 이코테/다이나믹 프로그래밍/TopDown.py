03f2d2f9865b8cfde3d8c0f8bd132a263edbe385
bokang03 <bogang12345@gmail.com>
2025-12-01 13:43:09 +0900

    피보나치 수열 : 탑다운(TopDown) 기본 예제

---
+++ 이코테/다이나믹 프로그래밍/TopDown.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    d = [0] * 100 # 한 번 계산된 결과를 저장(메모제이션)하는 리스트
+
+    def fibo(x):
+        if x == 1 or x == 2 :
+            return 1
+        if d[x] != 0 :
+            return d[x]
+        d[x] = fibo(x-1) + fibo(x-2)
+        return d[x]
+
+    print(fibo(99))
