e566c2850e2a6c1ee6bbb0c4d03bf7a247b6bc2d
bokang03 <bogang12345@gmail.com>
2025-12-05 16:19:10 +0900

    프로그래머스 lv0. 홀짝 구분하기

--- Programmers/lv0/문자열 붙여서 출력하기/README.md
+++ Programmers/lv0/문자열 붙여서 출력하기/README.md
@@ -23,3 +23,4 @@ Hello World!
 ```
 HelloWorld!
 ```
+
---
+++ Programmers/lv0/홀짝 구분하기/홀짝 구분하기.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+
+    if a % 2 == 0:
+        print(a, "is even")
+    else:
+        print(a, "is odd")
