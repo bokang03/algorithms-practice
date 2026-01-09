925cba327aa707cc73b50da8625e96af9dab0b45
bokang <bogang12345@gmail.com>
2026-01-09 22:40:45 +0900

    백준 silver2. 퇴사

---
+++ BaekJun Algorithm/silver3/퇴사/퇴사.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    dp = [0] * (n+1)
+
+    for i in range(n-1, -1, -1) :
+        if arr[i][0] + i > n :
+            dp[i] = dp[i+1]
+        else :
+            dp[i] = max(dp[i+1], arr[i][1] + dp[i + arr[i][0]]) # 이전값, 현재의 p + dp[i]
