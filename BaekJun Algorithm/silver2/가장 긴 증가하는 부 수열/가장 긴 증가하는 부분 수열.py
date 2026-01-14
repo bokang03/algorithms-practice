508a622ad07046a34650c8baf3153a5eb54e62ca
bokang <bogang12345@gmail.com>
2026-01-14 21:43:54 +0900

    백준 silver2. 가장 긴 증가하는 부분 수열

---
+++ BaekJun Algorithm/silver2/가장 긴 증가하는 부 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    dp = [1] * n
+
+    for i in range(1, n) :
+        for k in range(i) :
+            if arr[i] > arr[k] :
+                dp[i] = max(dp[i], dp[k] + 1)
+
+    print(max(dp))
