05ae5cf620f33668c6f945d7dabce489d1ffac60
bokang <bogang12345@gmail.com>
2026-01-26 13:57:10 +0900

    백준 silver3. 2 x n 타일링2

---
+++ BaekJun Algorithm/silver3/2 x n 타일링2/2 x n 타일링 2.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__' :
+    n = int(input())
+
+    dp = [0] * (n+1)
+    dp[0] = 1
+    dp[1] = 1
+
+    if n < 2 :
+        print(dp[n])
+    else :
+        dp[2] = 3
+        for i in range(2, n+1) :
+            dp[i] = dp[i-2] * 2 + dp[i-1]
+
+        print(dp[n]%10007)
