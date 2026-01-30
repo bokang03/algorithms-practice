f4f3069a3d064d4ed98f0e387aadb9b4b7f34631
bokang <bogang12345@gmail.com>
2026-01-31 01:05:40 +0900

    백준 silver1. 포도주 시식

---
+++ BaekJun Algorithm/silver1/포도주 시식/포도주 시식.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__" :
+    n = int(input())
+    arr = [int(input()) for _ in range(n)]
+
+    if n == 1 :
+        print(arr[0])
+    else :
+        dp = [0] * (n + 1)
+        dp[1] = arr[0]
+        dp[2] = arr[0] + arr[1]
+
+        for i in range(3, n+1) :
+            dp[i] = max(dp[i-3] + arr[i-2] + arr[i-1], dp[i-2] + arr[i-1], dp[i-1])
+
+        print(max(dp))
